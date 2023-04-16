b101a21cc14504960cfe341276cd6c3b88e5f88c
God Chama <mar21430@uvg.edu.gt>
2023-04-16 10:38:28 -0600

    COmmit

--- Query_Tool.py
+++ Query_Tool.py
@@ -22,19 +22,24 @@ class Query_Tool(QT_Text_Editor):
 				cur.execute(self.toPlainText())
 				conn.commit()
 				self.Data = cur.fetchall()
-				Coulmn_Labels = [str(desc[0]) for desc in cur.description]
-				self.Output.Spreadsheet.setColumnCount(len(Coulmn_Labels))
-				self.Output.Spreadsheet.setRowCount(len(self.Data))
-				self.Output.Spreadsheet.setHorizontalHeaderLabels(Coulmn_Labels)
+				try:
+					Coulmn_Labels = [str(desc[0]) for desc in cur.description]
+					self.Output.Set = True
+					self.Output.Spreadsheet.setColumnCount(len(Coulmn_Labels))
+					self.Output.Spreadsheet.setRowCount(len(self.Data))
+					self.Output.Spreadsheet.setHorizontalHeaderLabels(Coulmn_Labels)
 
-				for row in range(len(self.Data)):
-					for column in range(len(self.Data[0])):
-						item = QTableWidgetItem(self.Data[row][column])
-						self.Output.Spreadsheet.setItem(row, column, item)
+					for row in range(len(self.Data)):
+						for column in range(len(self.Data[0])):
+							item = QTableWidgetItem(self.Data[row][column])
+							self.Output.Spreadsheet.setItem(row, column, item)
 
-				self.Output.Spreadsheet.resizeColumnsToContents()
-				self.Output.Spreadsheet.resizeRowsToContents()
-				self.Log.append("Query executed succesfully!","150,250,150")
+					self.Output.Spreadsheet.resizeColumnsToContents()
+					self.Output.Spreadsheet.resizeRowsToContents()
+					self.Output.Set = False
+					self.Log.append("Query executed succesfully!","150,250,150")
+				except:
+					self.Output.refresh()
 
 			except sqlite3.Error as Error: 
 				self.Log.append("Error: " + str(Error),"250,50,50")
@@ -48,20 +53,22 @@ class Query_Tool(QT_Text_Editor):
 				cur.execute(self.textCursor().block().text())
 				conn.commit()
 				self.Data = cur.fetchall()
-				Coulmn_Labels = [str(desc[0]) for desc in cur.description]
-				self.Output.Spreadsheet.setColumnCount(len(Coulmn_Labels))
-				self.Output.Spreadsheet.setRowCount(len(self.Data))
-				self.Output.Spreadsheet.setHorizontalHeaderLabels(Coulmn_Labels)
+				try:
+					Coulmn_Labels = [str(desc[0]) for desc in cur.description]
+					self.Output.Spreadsheet.setColumnCount(len(Coulmn_Labels))
+					self.Output.Spreadsheet.setRowCount(len(self.Data))
+					self.Output.Spreadsheet.setHorizontalHeaderLabels(Coulmn_Labels)
 
-				for row in range(len(self.Data)):
-					for column in range(len(self.Data[0])):
-						item = QTableWidgetItem(self.Data[row][column])
-						self.Output.Spreadsheet.setItem(row, column, item)
+					for row in range(len(self.Data)):
+						for column in range(len(self.Data[0])):
+							item = QTableWidgetItem(self.Data[row][column])
+							self.Output.Spreadsheet.setItem(row, column, item)
 
-				self.Output.Spreadsheet.resizeColumnsToContents()
-				self.Output.Spreadsheet.resizeRowsToContents()
-				self.Log.append("Query Fragment executed succesfully!","150,250,150")
-
+					self.Output.Spreadsheet.resizeColumnsToContents()
+					self.Output.Spreadsheet.resizeRowsToContents()
+					self.Log.append("Query Fragment executed succesfully!","150,250,150")
+				except:
+					self.Output.refresh()
 			except sqlite3.Error as Error: 
 				self.Log.append("Error: " + str(Error),"250,50,50")
 
